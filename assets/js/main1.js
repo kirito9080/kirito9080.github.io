73e6b1693e055b4233e0bd91fad5a1690fad25f8
Whitestorys <zccbcxb@gmail.com>
2020-08-16 00:45:09 +0800

    update

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -1,3 +1,7 @@
+//----------------------------------------
+//Design By 白色の物语(Whitecolor)
+//Github:https://github.com/Whitestorys
+//----------------------------------------
 (function() {
 	"use strict";
 
